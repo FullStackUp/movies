b669b826ac8458487859b79283035e723c080240
FullStackUp <fullstackup.webdeveloper@gmail.com>
2022-07-07 21:53:43 +0200

    Setting up the MovieElement component

--- src/components/movie-list/MovieElement.js
+++ src/components/movie-list/MovieElement.js
@@ -2,6 +2,24 @@ import React, { Component } from "react";
 
 export default class MovieElement extends Component {
   render() {
-    return <div>Movie Element</div>;
+    return (
+      <div className="w-25 p-2">
+        <div className="card">
+          <img
+            alt="the batman"
+            src="https://fr.web.img5.acsta.net/pictures/22/02/16/17/42/3125788.jpg"
+            className="card-img-top"
+          />
+          <div className="card-body">
+            <h5 className="card-title">The Batman</h5>
+            <p className="card-text">
+              Two years of walking the streets as Batman and instilling fear in
+              criminals have brought Bruce Wayne to the heart of Gotham City's
+              darkness...
+            </p>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
